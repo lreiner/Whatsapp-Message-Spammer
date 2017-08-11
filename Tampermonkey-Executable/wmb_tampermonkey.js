5d014a7f35348dc0594a5c80b2ecff544a90d5fb
Lukas R <30616142+lukay97@users.noreply.github.com>
2017-08-11 12:22:18 +0200

    New Version 2.0 Final is out!
    -Design updates!
    -Donation Button added!
    -Bug fixes!

--- Tampermonkey-Executable/wmb_tampermonkey.js
+++ Tampermonkey-Executable/wmb_tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Whatsapp Message botton
+// @name         Whatsapp Message Bot
 // @namespace    https://github.com/re-invent/Whatsapp-Message-Bot
-// @version      1.0
+// @version      2.0
 // @description  Made by lukay97
 // @author       lukay97
 // @match        https://web.whatsapp.com/
@@ -17,10 +17,13 @@ function general(){
     var item2 = document.getElementsByClassName("pane-header pane-list-header")[0];
     var panel = document.getElementsByClassName("chatlist-panel pane pane-one")[0];
     var element = item2.cloneNode(true);
-    element.style.zIndex = 0;
-    element.innerHTML = "<input type='text' id='mensaje' placeholder='Message' style='margin-right:10px' size='30'><input type='number' min='1' id='repeticiones' style='width:50px;padding-left:5px;margin-right:5px'>Times<div id='spam' data-icon=\"send\" class=\"img icon icon-send\" style='margin-left: 5px;cursor:pointer;'><svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 24 24\" width=\"24\" height=\"24\"><path opacity=\".4\" d=\"M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z\"></path></svg></div>";
+    var element2 = item2.cloneNode(true);
+    element2.innerHTML = '<input type="text" id="mensaje" placeholder=" Message" style="margin-right:10px; width: 100%; border: solid 5px #c9c9c9; height: 30px; padding-left: 15px" size="30">';
+    element.innerHTML = '<button type="button" style="width: 100%; display: inline-block;"><img id="donate" src="http://i.imgur.com/Cs3BBYI.png" style="display: block; max-width: 95%; max-height: 100px; min-height: 55px;"></button><input type="number" min="1" id="repeticiones" style="width:50px;padding-left:5px; max-width: 80%; width: 80%; height: 30px; border: solid 5px #c9c9c9; margin-right: 10px">Times<div id="spam" data-icon="send" class="img icon icon-send" style="margin-left: 10px;margin-right: 15px;cursor:pointer;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path opacity=".4" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path></svg></div>';
     panel.insertBefore(element, panel.childNodes[1]);
+    panel.insertBefore(element2, panel.childNodes[1]);
     document.getElementById("spam").addEventListener("click", spam);
+    document.getElementById("donate").addEventListener("click", donate);
     clearInterval(timer);
   }else{
     console.log("WS Spam: Waiting for whatsapp to load...");
@@ -43,7 +46,13 @@ function spam(){
   var input = document.getElementsByClassName("input")[0];
   var counter = 1;
   while(counter <= reps){
-    dispatch(input, text); 									
+    dispatch(input, text);
     counter++;
   }
+}
+
+function donate(){
+  var url ="https://www.paypal.me/LukasReiner";
+  var win = window.open(url, '_blank');
+  win.focus();
 }
