68319d18e3124e63fff7a24c1573860de1f1340a
Lukas Reiner <lukasrein97@gmail.com>
2018-06-16 15:17:51 +0200

    Update Profile Information

--- Tampermonkey-Executable/wmb_tampermonkey.js
+++ Tampermonkey-Executable/wmb_tampermonkey.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Whatsapp Message Bot
-// @namespace    https://github.com/reinerluke97/Whatsapp-Message-Spammer/
+// @namespace    https://github.com/reinerluke/Whatsapp-Message-Spammer/
 // @version      2.0
-// @description  Made by reinerluke97
-// @author       reinerluke97
+// @description  Made by reinerluke
+// @author       reinerluke
 // @match        https://web.whatsapp.com/
 // @grant        none
 // @icon         https://k60.kn3.net/58A8A056B.png
