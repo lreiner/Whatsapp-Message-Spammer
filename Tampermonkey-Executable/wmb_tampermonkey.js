70bb2ad12fa71c1b6408dad345cbb70a57b93aed
Lukas Reiner <lukasrein97@gmail.com>
2018-06-30 14:02:40 +0200

    Bugfixes

--- Tampermonkey-Executable/wmb_tampermonkey.js
+++ Tampermonkey-Executable/wmb_tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Whatsapp Message Bot
 // @namespace    https://github.com/reinerluke/Whatsapp-Message-Spammer/
-// @version      2.0
+// @version      3.0
 // @description  Made by reinerluke
 // @author       reinerluke
 // @match        https://web.whatsapp.com/
@@ -12,15 +12,29 @@
 var timer = setInterval(general, 1000);
 
 function general() {
-    if (document.getElementsByClassName("app-wrapper-main")[0] != null) {
-        var item2 = document.getElementsByClassName("pane-header pane-list-header")[0];
-        var panel = document.getElementsByClassName("chatlist-panel pane pane-one")[0];
-        var element = item2.cloneNode(true);
-        var element2 = item2.cloneNode(true);
-        element2.innerHTML = '<input type="text" id="mensaje" placeholder=" Message" style="margin-right:10px; width: 100%; border: solid 5px #c9c9c9;height: 30px; padding-left: 15px" size="30">';
-        element.innerHTML = '<button type="button" style="width: 100%; display: inline-block;"><img id="donate" src="http://i.imgur.com/Cs3BBYI.png" style="display: block; max-width: 95%; max-height: 100px; min-height: 55px;"></button><input type="number" min="1" id="repeticiones" style="width:50px;padding-left:5px; max-width: 80%; width: 80%; height: 30px; border: solid 5px #c9c9c9; margin-right: 10px">Times<div id="spam"data-icon="send"class="imgiconicon-send"style="margin-left:10px;margin-right: 15px;cursor:pointer;"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path opacity=".4" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path></svg></div>';
-        panel.insertBefore(element, panel.childNodes[1]);
-        panel.insertBefore(element2, panel.childNodes[1]);
+    if (document.getElementsByClassName("m6ZEb")[0] != null) {
+        console.log("WS Spam: Found Div to inject ...");
+        var item = document.getElementsByClassName("m6ZEb")[0];
+        var element = item.cloneNode(true);
+        element.innerHTML = '<div style="height: 110px; width: 100%;"> \
+                                <input type="text" id="inputMessage" placeholder="Message ..." style="margin-left: 10px; margin-top: 10px; width: calc(100% - 50px); height: 30px; padding-left: 10px"> \
+                                <hr style="border-color: white; color: #dbdbdb; background-color: #dbdbdb; border: none; height: 1px;"> \
+                                <div style="padding-left: 10px;"> \
+                                    <div data-icon="send" class="imgiconicon-send" style="cursor:pointer; height: 36px; display: inline-block; width: 100%;"> \
+                                        <input type="number" min="1" id="timesSend" placeholder="Times ..." style="width:50px;padding-left: 10px;max-width: 40%;width: 80%;height: 30px;margin-right: 10px;float:left;"> \
+                                        <button type="button" id="spam" style="background-color: transparent; color: transparent;"> \
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="36" height="36" style="display: inline"> \
+                                                <path opacity=".4" d="M1.101 21.757L23.8 12.028 1.101 2.3l.011 7.912 13.623 1.816-13.623 1.817-.011 7.912z"></path> \
+                                            </svg> \
+                                        </button> \
+                                        <button type="button" id="donate" style="display: inline; float:right; margin-right: 30px;"> \
+                                            <img src="http://i.imgur.com/Cs3BBYI.png" style="max-width: 100px; max-height: 36px; min-height: 36px;"> \
+                                        </button> \
+                                    </div>  \
+                                </div> \
+                            </div>';
+        item.appendChild(element);
+        console.log("WS Spam: Injected successfully ...");
         document.getElementById("spam").addEventListener("click", spam);
         document.getElementById("donate").addEventListener("click", donate);
         clearInterval(timer);
@@ -30,19 +44,19 @@ function general() {
 }
 
 function dispatch(input, message) {
-    InputEvent = Event || InputEvent;
+    //InputEvent = Event || InputEvent;
     var evt = new InputEvent('input', {
         bubbles: true
     });
     input.innerHTML = message;
     input.dispatchEvent(evt);
-    document.querySelector(".compose-btn-send > .icon-send").click();
+    document.querySelector("._2lkdt").click();
 }
 
 function spam() {
-    var text = document.getElementById("mensaje").value;
-    var reps = document.getElementById("repeticiones").value;
-    var input = document.getElementsByClassName("input")[0];
+    var text = document.getElementById("inputMessage").value;
+    var reps = document.getElementById("timesSend").value;
+    var input = document.getElementsByClassName("_2S1VP copyable-text selectable-text")[0];
     var counter = 1;
     while (counter <= reps) {
         dispatch(input, text);
